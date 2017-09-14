cb70b767c7cb6f045a14aa8654c7f7000ce2ff0b
Alexander Durbin <ajd2@vt.edu>
2017-09-14 17:07:16 -0400

    Changed file names and removed unnecessary files for hwk2

--- hwk/hwk2/hw2.R
+++
@@ -1,8 +0,0 @@
-## ----setup, include=FALSE------------------------------------------------
-knitr::opts_chunk$set(echo = TRUE)
-
-## ---- eval=FALSE---------------------------------------------------------
-## 
-## knitr::knit(input = my_file, tangle = TRUE)
-## 
-
