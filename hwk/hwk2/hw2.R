58142a0afd53b664e2455fb472e4d4c31409b988
Alexander Durbin <ajd2@vt.edu>
2017-09-14 17:04:32 -0400

    Completed #2 and refined #4. TODO: Add new commands to unix.txt

---
+++ hwk/hwk2/hw2.R
@@ -0,0 +1,8 @@
+## ----setup, include=FALSE------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
+
+## ---- eval=FALSE---------------------------------------------------------
+## 
+## knitr::knit(input = my_file, tangle = TRUE)
+## 
+
