9cc7240414fe1804a605914102f2e1eced72d03d
Alexander Durbin <ajd2@vt.edu>
2017-09-20 09:30:15 -0400

    Begin modifying plot method, variable names have been changed so far

--- hwk/hwk2/bisection.R
+++ hwk/hwk2/bisection.R
@@ -23,26 +23,27 @@ bisection <- function(f, xl, xr, tol=sqrt(.Machine$double.eps),
   else if(fl * fr > 0){
     
     if(verb > 0 ) cat("Warning: fl * fr > 0\nIterating to expand interval\n")
-    iter <- 0
+    i <- 0
+    # expand interval until fl * fr > 0 or max iterations reached
     while(fl * fr > 0){
       
-      iter <- iter + 1
+      i <- i + 1
       xm <- (xr + xl)/2
       w <- xr - xl
       xl <- xm - w
       xr <- xm + w
       fl <- f(xl)
       fr <- f(xr)
-      out$int_prog[iter,] <- c(xl, xr, fl, fr)
+      out$int_prog[i,] <- c(xl, xr, fl, fr)
       
       if(verb > 0)
-        cat("iter=", iter, ", (xl, xr)=(", xl, ", ", xr, ")\n", sep="")
+        cat("i=", i, ", (xl, xr)=(", xl, ", ", xr, ")\n", sep="")
       
-      if(iter >= maxiter) { stop('Max bracket iterations reached') }
+      if(i >= maxiter) { stop('Max bracket iterations reached') }
       
     }  
     
-    out$brckt_iter <- iter
+    out$brckt_iter <- i
     
   }
     
@@ -80,12 +81,14 @@ bisection <- function(f, xl, xr, tol=sqrt(.Machine$double.eps),
 print.bisection <- function(x, ...) {
   
   if(x$brckt_iter > 0){
+    cat("Original interval ", x$int_prog$xl[1], ", ", x$int_prog$xr[1], 
+        "expanded to ", tail(x$int_prog$xl, n = 1), ", ", tail(x$int_prog$xr, n = 1), "\n")
     cat("Root of:\n")
     print(x$f)
     cat("in (", tail(x$int_prog$xl, n = 1), ", ", tail(x$int_prog$xr, n = 1),
         ") found after ", nrow(x$root_prog), " iterations: ",
         x$ans, "\n", "to a tolerance of ", x$tol, "\n", sep="") 
-    cat("Took ", x$brckt_iter," iterations to expand interval for root\n")
+    cat("Took ", nrow(x$int_prog)," iterations to expand interval for root\n")
   } else {
     
     cat("Root of:\n")
@@ -105,9 +108,9 @@ print.bisection <- function(x, ...) {
 summary.bisection <- function(object, ...)
 {
   print(object, ...)
-  cat("\nInterval progress is as follows\n")
+  cat("\nInterval expansion progress is as follows\n")
   print(object$int_prog)
-  cat("\nRoot progress is as follows\n")
+  cat("\nRoot finding progress is as follows\n")
   print(object$root_prog)
 }
 
@@ -120,30 +123,30 @@ summary.bisection <- function(object, ...)
 plot.bisection <- function(object, gridlen=1000, after=NULL, ...)
 {
   ## check if there is something to do
-  if(nrow(object$prog) == 0) {
+  if(nrow(object$root_prog) == 0) {
     cat("nothing to plot\n")
     invisible(NULL)
   }
   
   ## check after
   if(!is.null(after)) {
-    if(after < 0 || after >= nrow(object$prog))
+    if(after < 0 || after >= nrow(object$root_prog))
       stop("after must be in 1:(nrow(objext$prog)-1)")
-    object$prog <- object$prog[after:nrow(object$prog),]
+    object$root_prog <- object$root_prog[after:nrow(object$root_prog),]
   } else after <- 0
   
   ## plots the true function
-  xgrid <- seq(object$prog$xl[1], object$prog$xr[1], length=gridlen)
-  xgrid <- sort(union(xgrid, c(object$prog$xl, object$prog$xr)))
+  xgrid <- seq(object$root_prog$xl[1], object$root_prog$xr[1], length=gridlen)
+  xgrid <- sort(union(xgrid, c(object$root_prog$xl, object$root_prog$xr)))
   y <- object$f(xgrid) ## assumes a vectorized function
   plot(xgrid, y, type="l", col="gray", ...)
   
   ## indicates (only) the changes in xl and xr
-  n <- nrow(object$prog)
-  dl <- duplicated(object$prog$xl)
-  text(object$prog$xl[!dl], object$prog$fl[!dl], ((1:n)+after)[!dl])
-  dr <- duplicated(object$prog$xr)
-  text(object$prog$xr[!dr], object$prog$fr[!dr], ((1:n)+after)[!dr], col=2)
+  n <- nrow(object$root_prog)
+  dl <- duplicated(object$root_prog$xl)
+  text(object$root_prog$xl[!dl], object$root_prog$fl[!dl], ((1:n)+after)[!dl])
+  dr <- duplicated(object$root_prog$xr)
+  text(object$root_prog$xr[!dr], object$root_prog$fr[!dr], ((1:n)+after)[!dr], col=2)
   
   ## adds a reference line
   abline(h=0, col=3, lty=3)
