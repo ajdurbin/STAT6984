6f78a6c9dc4a64b64f34bc0e15bc4bb5014f9258
Alexander Durbin <ajd2@vt.edu>
2017-10-02 11:59:21 -0400

    Shell script working, fixed goal differential, added a little more writeup

--- hwk/hwk3/hockey.R
+++ hwk/hwk3/hockey.R
@@ -1,4 +1,7 @@
-library(tidyverse)
+suppressWarnings(suppressMessages(
+  library(tidyverse, warn.conflicts = FALSE, quietly = TRUE)))
+
+options(stringsAsFactors = FALSE)
 
 # function declarations ---------------------------------------------------
 
@@ -11,69 +14,71 @@ combo_all <- function(web, key){
   
   # this appends home team information to columns, still want visitor stuff
   # so need to merge again with different key
-  total <- merge(x = web, y = key, by.x = "Home", by.y = "Team")
-  total <- merge(x = total, y = key, by.x = "Visitor", by.y = "Team")
+  all_data <- merge(x = web, y = key, by.x = "Home", by.y = "Team")
+  all_data <- merge(x = all_data, y = key, by.x = "Visitor", by.y = "Team")
   # rename columns since second merge causes .1 for repeated names
-  colnames(total) <- c("Visitor", "Home", "Date", "VG", "HG", "TYPE",
+  colnames(all_data) <- c("Visitor", "Home", "Date", "VG", "HG", "TYPE",
                        "Att.", "LOG", "Notes", "HABR", "HCONF", "HDIV",
                        "VABR", "VCONF", "VDIV")
   
   # transform NA values in type to 'reg'
-  total <- transform(total, TYPE = ifelse(is.na(TYPE), "REG", TYPE))
-  total <- transform(total, TYPE = ifelse(is.na(TYPE), "REG", TYPE))
+  all_data <- transform(all_data, TYPE = ifelse(is.na(TYPE), "REG", TYPE))
+  all_data <- transform(all_data, TYPE = ifelse(is.na(TYPE), "REG", TYPE))
   
   # add new columns for wins based on home/away
-  total <- transform(total, HW = ifelse(HG>VG, 1, 0))
-  total <- transform(total, VW = ifelse(VG>HG, 1, 0))
+  all_data <- transform(all_data, HW = ifelse(HG>VG, 1, 0))
+  all_data <- transform(all_data, VW = ifelse(VG>HG, 1, 0))
   
   # add new columns for goals for, goals against by home/away
-  total$HGF <- 0
-  total$HGA <- 0
-  total$VGF <- 0
-  total$VGA <- 0
+  all_data$HGF <- 0
+  all_data$HGA <- 0
+  all_data$VGF <- 0
+  all_data$VGA <- 0
   
   # nonshootout games
   # goals for and against are unchaged
-  total <- transform(total, HGF = ifelse(TYPE != "SO", HG, 0))
-  total <- transform(total, HGA = ifelse(TYPE != "SO", VG, 0))
-  total <- transform(total, VGF = ifelse(TYPE != "SO", VG, 0))
-  total <- transform(total, VGA = ifelse(TYPE != "SO", HG, 0))
+  all_data <- transform(all_data, HGF = ifelse(TYPE != "SO", HG, 0))
+  all_data <- transform(all_data, HGA = ifelse(TYPE != "SO", VG, 0))
+  all_data <- transform(all_data, VGF = ifelse(TYPE != "SO", VG, 0))
+  all_data <- transform(all_data, VGA = ifelse(TYPE != "SO", HG, 0))
   
   # shootout has two cases
   # for wins
-  total <- transform(total, HGF = ifelse(HW == 1 & TYPE == "SO", HG - 1, HGF))
-  total <- transform(total, HGA = ifelse(HW == 1 & TYPE == "SO", VG, HGA))
-  total <- transform(total, VGF = ifelse(HW == 1 & TYPE == "SO", VG, VGF))
-  total <- transform(total, VGA = ifelse(HW == 1 & TYPE == "SO", HG - 1, VGA))
+  all_data <- transform(all_data, HGF = ifelse(HW == 1 & TYPE == "SO", HG - 1, HGF))
+  all_data <- transform(all_data, HGA = ifelse(HW == 1 & TYPE == "SO", VG, HGA))
+  all_data <- transform(all_data, VGF = ifelse(HW == 1 & TYPE == "SO", VG, VGF))
+  all_data <- transform(all_data, VGA = ifelse(HW == 1 & TYPE == "SO", HG - 1, VGA))
   
   # for losses
-  total <- transform(total, HGF = ifelse(HW == 0 & TYPE == "SO", HG, HGF))
-  total <- transform(total, HGA = ifelse(HW == 0 & TYPE == "SO", VG - 1, HGA))
-  total <- transform(total, VGF = ifelse(HW == 0 & TYPE == "SO", VG - 1, VGF))
-  total <- transform(total, VGA = ifelse(HW == 0 & TYPE == "SO", HG, VGA))
+  all_data <- transform(all_data, HGF = ifelse(HW == 0 & TYPE == "SO", HG, HGF))
+  all_data <- transform(all_data, HGA = ifelse(HW == 0 & TYPE == "SO", VG - 1, HGA))
+  all_data <- transform(all_data, VGF = ifelse(HW == 0 & TYPE == "SO", VG - 1, VGF))
+  all_data <- transform(all_data, VGA = ifelse(HW == 0 & TYPE == "SO", HG, VGA))
   
   # goal diff
-  total <- transform(total, HGD = HGF - HGA)
-  total <- transform(total, VGD = VGF - VGA)
+  all_data <- transform(all_data, HGD = HGF - HGA)
+  all_data <- transform(all_data, VGD = VGF - VGA)
   
   
   # now add points for ot/so wins
-  total$HPTS <- 0
-  total$VPTS <- 0
+  all_data$HPTS <- 0
+  all_data$VPTS <- 0
   
-  total <- transform(total, HPTS = ifelse(HW == 1, 2, HPTS))
-  total <- transform(total, VPTS = ifelse(VW == 1, 2, VPTS))
-  total <- transform(total, HPTS = ifelse(HW == 0 & (TYPE == "SO" | TYPE == "OT"), 1, HPTS))
-  total <- transform(total, VPTS = ifelse(VW == 0 & (TYPE == "SO" | TYPE == "OT"), 1, VPTS))
+  all_data <- transform(all_data, HPTS = ifelse(HW == 1, 2, HPTS))
+  all_data <- transform(all_data, VPTS = ifelse(VW == 1, 2, VPTS))
+  all_data <- transform(all_data, HPTS = ifelse(HW == 0 & (TYPE == "SO" | TYPE == "OT"), 1, HPTS))
+  all_data <- transform(all_data, VPTS = ifelse(VW == 0 & (TYPE == "SO" | TYPE == "OT"), 1, VPTS))
   
-  return(total)
+  return(all_data)
   
 }
 
 # take all game data and reduce to team data
 combo_by_team <- function(all_data){
   
-  names <- unique(cbind(total$Visitor, total$VABR, total$VCONF, total$VDIV))
+  names <- unique(cbind(all_data$Visitor, all_data$VABR, all_data$VCONF,
+                        all_data$VDIV))
+  
   by_team <- data.frame(team = names[, 1],
                         abr = names[, 2],
                         conf = names[, 3],
@@ -89,26 +94,29 @@ combo_by_team <- function(all_data){
   # fill in data
   for (tm in names[, 1]){
     
-    by_team[by_team$team == tm, ]$gp = nrow(total[total$Visitor == tm, ]) +
-      nrow(total[total$Home == tm, ])
+    by_team[by_team$team == tm, ]$gp = nrow(all_data[all_data$Visitor == tm, ]) +
+      nrow(all_data[all_data$Home == tm, ])
     
-    by_team[by_team$team == tm, ]$w = nrow(total[total$Visitor == tm & total$VW == 1, ]) +
-      nrow(total[total$Home == tm & total$HW == 1, ])
+    by_team[by_team$team == tm, ]$w = nrow(all_data[all_data$Visitor == tm & all_data$VW == 1, ]) +
+      nrow(all_data[all_data$Home == tm & all_data$HW == 1, ])
     
-    by_team[by_team$team == tm, ]$l = nrow(total[total$Visitor == tm & total$VW == 0, ]) +
-      nrow(total[total$Home == tm & total$HW == 0, ])
+    by_team[by_team$team == tm, ]$l = nrow(all_data[all_data$Visitor == tm & all_data$VW == 0, ]) +
+      nrow(all_data[all_data$Home == tm & all_data$HW == 0, ])
     
-    by_team[by_team$team == tm, ]$pts = sum(total[total$Visitor == tm, ]$VPTS) +
-      sum(total[total$Home == tm, ]$HPTS)
+    by_team[by_team$team == tm, ]$pts = sum(all_data[all_data$Visitor == tm, ]$VPTS) +
+      sum(all_data[all_data$Home == tm, ]$HPTS)
     
-    by_team[by_team$team == tm, ]$gf = sum(total[total$Visitor == tm, ]$VGF) +
-      sum(total[total$Home == tm, ]$HGF)
+    by_team[by_team$team == tm, ]$gf = sum(all_data[all_data$Visitor == tm, ]$VGF) +
+      sum(all_data[all_data$Home == tm, ]$HGF)
     
-    by_team[by_team$team == tm, ]$ga = sum(total[total$Visitor == tm, ]$VGA) +
-      sum(total[total$Home == tm & total$HW == 1, ]$HGA)
+    by_team[by_team$team == tm, ]$ga = sum(all_data[all_data$Visitor == tm, ]$VGA) +
+      sum(all_data[all_data$Home == tm & all_data$HW == 1, ]$HGA)
     
-    by_team[by_team$team == tm, ]$diff = sum(total[total$Visitor == tm, ]$VGD) +
-      sum(total[total$Home == tm & total$HW == 1, ]$HGD)
+    # by_team[by_team$team == tm, ]$diff = sum(all_data[all_data$Visitor == tm, ]$VGD) +
+    #   sum(all_data[all_data$Home == tm & all_data$HW == 1, ]$HGD)
+    
+    by_team[by_team$team == tm, ]$diff = by_team[by_team$team == tm, ]$gf -
+      by_team[by_team$team == tm, ]$ga
     
   }
   
@@ -135,7 +143,7 @@ team_print <- function(team_data, abr = "", conf = "", div = ""){
       
       cat("-----", "\nConference: ", c, "\nDivision: ", d, "\nStandings: \n")
       print(the_div[with(the_div, order(pts, -l, w, diff, decreasing = TRUE)), ], row.names = FALSE)
-      cat("-----")
+      cat("-----\n")
       
     }
     
---
+++ hwk/hwk3/nhlstandings.sh
@@ -0,0 +1,1 @@
+Rscript -e "source('hockey.R')"
