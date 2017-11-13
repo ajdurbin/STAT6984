58fbac6b6121dfd89679463885a49f09b84d6cec
Alexander Durbin <ajd2@vt.edu>
2017-11-13 17:39:33 -0500

    working on rcpp loglikes, stuck on matrix matrix multu multiplication

--- hwk/hwk5/rcpp/logliks_Rcpp.cpp
+++ hwk/hwk5/rcpp/logliks_Rcpp.cpp
@@ -3,21 +3,33 @@ using namespace Rcpp;
 
 // [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::export]]
-arma::vec logliks_Rcpp(arma::mat Y,arma::mat D, double theta){
+double logliks_Rcpp(arma::mat Y, arma::mat D, double theta,
+    double precision){
    
   int m = D.n_rows;
   arma::mat Sigma;
   arma::mat tmp1(m,m);
   tmp1.eye();
-  tmp1 = tmp1 * sqrt(0.0000000001);
+  tmp1 = tmp1 * sqrt(precision);
   theta = 1 / theta;
   arma::mat tmp2 = -D*theta;
   Sigma = exp(tmp2) + tmp1;
   arma::mat Schol;
   Schol = arma::chol(Sigma);
-  arma::vec ldet;
-  ldet = 2*arma::sum(log(diagmat(Schol)));
-  return(ldet);
+  arma::mat diagschol;
+  diagschol = arma::diagvec(Schol);
+  log(diagschol);
+  double ldet;
+  ldet = arma::accu(diagschol);
+  ldet = 2*ldet;
+  arma::mat Si;
+  Si = arma::inv(Schol);
+  int n = Y.n_rows;
+  double ll;
+  ll = -0.5 * n * (m * log(2 * arma::datum::pi) + ldet);
+  arma::mat row;
+  row = Y.row(1);
+  return(ll - 0.5 * row * Si * row.t());
    
 }
 
@@ -29,7 +41,7 @@ Sigma <- exp(-D) + diag(sqrt(.Machine$double.eps), nrow(D))
 library(mvtnorm)
 Y <- rmvnorm(10000, sigma=Sigma)
 print("before")
-test <- logliks_Rcpp(Y,D,thetas[1])
+test <- logliks_Rcpp(Y,D,thetas[1], .Machine$double.eps)
 print(test)
 print(class(test))
 print(dim(test))
