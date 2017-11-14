0edfe7f7c5cef5b8cd062e9daf58684cdef8a84c
Alexander Durbin <ajd2@vt.edu>
2017-11-14 10:50:35 -0500

    new test files for rcpp code

---
+++ hwk/hwk5/rcpp/mvnllik/logliks_Rcpp.cpp
@@ -0,0 +1,52 @@
+#include <RcppArmadillo.h>
+#include <omp.h>
+// [[Rcpp::plugins(openmp)]]
+using namespace Rcpp;
+
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::export]]
+arma::vec logliks_Rcpp(arma::mat Y, arma::mat D, arma::vec thetas,
+    double precision){
+    int tlen = thetas.n_elem;
+    int m = D.n_rows;
+    int n = Y.n_rows;
+    arma::vec likes(tlen);
+    for(int i = 0; i < tlen; i++){
+      double theta = thetas(i);
+      arma::mat Sigma = exp(-D/theta);
+      arma::mat Schol = arma::chol(Sigma);
+      double ldet = 2 * arma::accu(arma::log(Schol.diag()));
+      arma::mat Si = arma::inv_sympd(Sigma);
+      double ll = -0.5 * n * (m * log(2 * arma::datum::pi) + ldet); 
+      for(int j = 0; j < n; j++){
+          ll -= arma::as_scalar(0.5 * Y.row(j) * Si * arma::trans(Y.row(j)));
+      }
+      likes(i) = ll;
+    }  
+    return(likes);
+}
+
+
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::export]]
+arma::vec logliks_Rcpp_omp(arma::mat Y, arma::mat D, arma::vec thetas,
+    double precision){
+    int tlen = thetas.n_elem;
+    int m = D.n_rows;
+    int n = Y.n_rows;
+    arma::vec likes(tlen);
+    for(int i = 0; i < tlen; i++){
+        double theta = thetas(i);
+        arma::mat Sigma = exp(-D/theta);
+        arma::mat Schol = arma::chol(Sigma);
+        double ldet = 2 * arma::accu(arma::log(Schol.diag()));
+        arma::mat Si = arma::inv_sympd(Sigma);
+        double ll = -0.5 * n * (m * log(2 * arma::datum::pi) + ldet); 
+        for(int j = 0; j < n; j++){
+            ll -= arma::as_scalar(0.5 * Y.row(j) * Si * arma::trans(Y.row(j)));
+        }
+        likes(i) = ll;
+    }  
+    return(likes);
+}
+
